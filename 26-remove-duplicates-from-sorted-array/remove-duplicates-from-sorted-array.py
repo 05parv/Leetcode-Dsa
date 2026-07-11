ff95a49c9938908901295867807ba643047b617b
Parv Jain <parvjainbpl@outlook.com>
2026-07-11 21:28:36 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (42.00%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n=len(nums)
+        hash_map={}
+        for i in range(0,n):
+            hash_map[nums[i]]=0
+        j=0
+        for k in  hash_map:
+            nums[j]=k
+            j+=1
+        return j
